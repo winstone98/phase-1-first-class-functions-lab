abdb0718e80e4f2a73e326b8b13b9ecee6bfbcd6
root <root@HomeLPT-001.localdomain>
2022-06-09 23:39:56 +0300

    Just done with it

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (Listdrivers) {
+    return Listdrivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (passDriversList) {
+    return passDriversList.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (fareRate) {
+    return function (fareRank) {
+        return fareRate * fareRank;
+    };
+};
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, returnDrivers) {
+    return returnDrivers(drivers);
+};
